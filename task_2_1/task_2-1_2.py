aeb53ca94ace465da249f1ce61094d2ad29f31b6
maslennikova9b-ux <maslennikova.9b@gmail.com>
2026-03-05 11:07:17 +0300

    Delete task_2_1 directory

--- task_2_1/task_2-1_2.py
+++
@@ -1,9 +0,0 @@
-# task_2-1_2.py
-# Отчет о версии Python
-
-# Создаем переменную с версией Python
-python_version = "3.10"
-
-# Выводим информацию с форматированием
-print(f"Language: Python")
-print(f"\tVersion: {python_version}")
